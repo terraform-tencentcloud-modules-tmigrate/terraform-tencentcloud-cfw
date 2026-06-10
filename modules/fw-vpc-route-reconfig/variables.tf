ac34fdd9bcdb2c3b94f1f433f7d791bf0f4d215a
MaDongdong <donaldma@tencent.com>
2026-06-10 19:33:13 +0800

    feat: add ccn_fw_type to control route republish filter (VPC/NAT) in fw-vpc-route-reconfig

--- modules/fw-vpc-route-reconfig/variables.tf
+++ modules/fw-vpc-route-reconfig/variables.tf
@@ -7,3 +7,13 @@ variable "gateway_id" {
   description = "(Required, String) Gateway ID of VPC firewall."
   type        = string
 }
+
+variable "ccn_fw_type" {
+  description = "(Optional, String) CCN firewall type. VPC: republish non-default routes; NAT: republish default route only. Valid values: VPC, NAT."
+  type        = string
+  default     = "VPC"
+  validation {
+    condition     = contains(["VPC", "NAT"], var.ccn_fw_type)
+    error_message = "ccn_fw_type must be 'VPC' or 'NAT'."
+  }
+}
